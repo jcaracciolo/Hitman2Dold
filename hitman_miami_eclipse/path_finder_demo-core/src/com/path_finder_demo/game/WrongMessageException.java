19f8ba611ec817b6fc62619a436d442acb6ecebb
traies <traies@itba.edu.ar>
2015-10-17 15:07:51 -0300

    Excepcion WrongMessageException

---
+++ hitman_miami_eclipse/path_finder_demo-core/src/com/path_finder_demo/game/WrongMessageException.java
@@ -0,0 +1,15 @@
+/*
+ *@author Tomas Raies
+ *@date   17 de oct. de 2015
+ */
+
+package com.path_finder_demo.game;
+
+public class WrongMessageException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
